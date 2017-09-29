21937682b7f7665a9bb0facc71ae721a2d410880
luoshao23 <luoshao23@gmail.com>
2017-09-29 13:35:28 +0800

    version 0.1

---
+++ blog/models.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.six import python_2_unicode_compatible
+
+
+@python_2_unicode_compatible
+class Category(models.Model):
+    """
+    https://docs.djangoproject.com/en/1.10/ref/models/fields/#field-types
+    """
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+@python_2_unicode_compatible
+class Tag(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible
+class Post(models.Model):
+    """Post"""
+    title = models.CharField(max_length=70)
+
+    body = models.TextField()
+
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+
+    # 文章摘要
+    excerpect = models.CharField(max_length=200, blank=True)
+
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    author = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    class Meta:
+        ordering = ['-created_time', 'title']
---
+++ comments/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils.six import python_2_unicode_compatible
+# Create your models here.
+
+@python_2_unicode_compatible
+class Comment(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=255)
+    url = models.URLField(blank=True)
+    text = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    post = models.ForeignKey('blog.Post')
+
+    def __str__(self):
+        return self.text[:20]
